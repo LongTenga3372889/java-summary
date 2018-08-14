a1903bf961cd11f9695f6423d811d547b4b369e1
龙腾 <teng.long@hand-china.com>
2018-08-14 15:09:10 +0800

    a

---
+++ jvm/src/VirtualMemory.java
@@ -0,0 +1,29 @@
+/**
+ * @author teng.long@hand-china.com
+ * @Name VirtualMemory
+ * @Description
+ * @Date 2018/8/14
+ */
+public class VirtualMemory {
+
+    private static int count = 0;
+
+    public static void main(String[] args) {
+        VirtualMemory virtualMemory = new VirtualMemory();
+        virtualMemory.test();
+    }
+
+    /**
+     * 虚拟机栈内存溢出
+     */
+    public void test(){
+        try {
+            count++;
+            test();
+        }catch (Throwable e){
+            System.out.println("递归次数："+count);
+            e.printStackTrace();
+        }
+    }
+
+}
