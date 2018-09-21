71a5d071ef6c188411a32c27623b18a81d06b379
龙腾 <teng.long@hand-china.com>
2018-09-21 15:29:30 +0800

    no message

--- jvm/src/structure/node/Node.java
+++ jvm/src/structure/node/Node.java
@@ -2,23 +2,20 @@ package structure.node;
 
 import error.exception.MyException;
 
+import java.io.*;
+
 /**
  * @author teng.long@hand-china.com
  * @Name Node
  * @Description
  * @Date 2018/9/20
  */
-public class Node {
+public class Node implements Serializable{
 
     /**
      * 链表数据
      */
     private String data;
-
-    /**
-     * 链表长度
-     */
-    private int length;
 
     /**
      * 下一个节点
@@ -32,38 +29,117 @@ public class Node {
 
     /**
      * 构造方法初始化时直接创建头节点和第一个数据
+     *
      * @param data
      */
-    public Node(String data){
-        this.data= data;
-        head=this;
-        length=1;
+    public Node(String data) {
+        this.data = data;
+        head = this;
     }
 
     /**
      * 在链表末尾加一个节点
+     *
      * @param node
      */
-    public void addNode(Node node){
+    public void addNode(Node node) {
         Node temp = head;
-        while(temp.next!=null){
-            temp=temp.next;
+        while (temp.next != null) {
+            temp = temp.next;
         }
-        temp.next=node;
-        temp.length = next.length()+node.length();
+        temp.next = node;
     }
 
-    public void insertNodeByIndex(int index,Node node){
-        if(index>length()+1||index<1){
+    @Override
+    public Node clone() {
+        ByteArrayOutputStream bo = new ByteArrayOutputStream();
+        ObjectOutputStream oo = null;
+        ByteArrayInputStream bi = null;
+        ObjectInputStream oi = null;
+        try {
+            oo = new ObjectOutputStream(bo);
+            oo.writeObject(this);
+            bi = new ByteArrayInputStream(bo.toByteArray());
+            oi = new ObjectInputStream(bi);
+            return (Node) oi.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if(oi!=null) {
+                    oi.close();
+                }if(bi!=null) {
+                    bi.close();
+                }if(oo!=null) {
+                    oo.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        throw new MyException("clone出现异常！");
+    }
+
+    /**
+     * 在指定的位子插入链表
+     *
+     * @param index
+     * @param node
+     */
+    public void insertNodeByIndex(int index, Node node) {
+        if (index > length() + 1 || index < 1) {
             throw new MyException("数组下标越界！");
+        }
+        Node temp = head;
+        //为了不影响node链表
+        Node node1 = node.clone();
+        while (temp.next != null) {
+            if (index == temp.length()) {
+                node1.addNode(temp.next);
+                temp.next = node1;
+                break;
+            }
+            temp = temp.next;
+        }
+    }
+
+    /**
+     * 删除指定位子的节点
+     *
+     * @param index
+     */
+    public void deleteNode(int index) {
+        if (index < 1 || index > length()) {
+            throw new MyException("数组下标越界！");
+        }
+        Node temp = head;
+        int length = 1;
+        while (temp.next != null) {
+            if (index == length++) {
+                temp.next = temp.next.next;
+            }
+            temp = temp.next;
         }
     }
 
     /**
      * 获取链表长度
+     *
      * @return
      */
-    public int length(){
+    public int length() {
+        int length = 0;
+        Node temp = head;
+        try {
+            while (temp.next != null) {
+                length++;
+                temp = temp.next;
+            }
+        } catch (Exception e) {
+            throw new MyException("链表头尾重合！");
+        }
         return length;
     }
 
@@ -72,7 +148,10 @@ public class Node {
         Node node2 = new Node("c");
         node2.addNode(new Node("b"));
         node1.addNode(new Node("b"));
-        node1.addNode(node2);
+        node1.insertNodeByIndex(1, node2);
+        node2.insertNodeByIndex(1, node1);
+        node2.deleteNode(2);
+        node1.deleteNode(2);
     }
 
 }
